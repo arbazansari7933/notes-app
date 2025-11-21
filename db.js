bc716e970333ab01a8ddef7f11ad65ce93caa4c3
Arbaz Ansari <arbazansari7933@gmail.com>
2025-11-22 01:55:28 +0530

    Mongo Atlas Connected

--- db.js
+++ db.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect("mongodb://127.0.0.1:27017/notesDB")
-  .then(() => console.log("MongoDB Connected"))
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Atlas Connected"))
   .catch((err) => console.log("DB Error:", err));
